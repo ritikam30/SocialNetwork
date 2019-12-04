dd3931c4d18a4b2a56bb38b3303f5ea41769efea
Erik Tiedt <etiedt@wisc.edu>
2019-12-04 08:02:21 -0600

    Additions to Person Class
    Added some fields and methods to the person class. May need more?

--- a101_Project/application/Person.java
+++ a101_Project/application/Person.java
@@ -1,7 +1,26 @@
 package application;
 
+import java.util.Set;
+
 public class Person {
   
   private String name;
-
+  private Set<Person> friends;
+  
+  public String getName() {
+    return name;
+  }
+  
+  public Set<Person> getFriends() {
+    return friends;
+  }
+  
+  public void addFriend(Person friend) {
+      friends.add(friend);
+  }
+  
+  public void removeFriend(Person friend) {
+      friends.remove(friend);
+  }
+  
 }
